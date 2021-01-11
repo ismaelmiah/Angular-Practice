426111972a0388b23ef8a2ebcc6c9e383ec7b954
ismail5g <ismail27.dec@gmail.com>
2021-01-11 13:30:38 +0600

    Services, Routing, Guard, Children Guard, Redirect To Error Page, Resolver

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth-service';
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    return this.canActivate(route, state);
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    return this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) return true;
+      else {
+        this.router.navigate(['/']);
+      }
+    });
+  }
+}
