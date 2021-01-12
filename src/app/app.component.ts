2abbccd56e976d5ab04df38cafd63bce4ecbb776
ismail5g <ismail27.dec@gmail.com>
2021-01-13 00:16:53 +0600

    Subject for Cross Componts, In normal EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  private activatedSub: Subscription;
 
   constructor(private userService: UserService) {}
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(didActivated => {
-      this.userActivated = didActivated;
-    })
+    this.activatedSub = this.userService.activatedEmitter.subscribe(
+      (didActivated) => {
+        this.userActivated = didActivated;
+      }
+    );
   }
 }
