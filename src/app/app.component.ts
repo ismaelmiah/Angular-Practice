45ac6beb453ed888ce504c0bbf0286068227141a
ismail5g <ismail27.dec@gmail.com>
2021-01-13 10:34:14 +0600

    Forms Started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { UserService } from './user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private activatedSub: Subscription;
-
-  constructor(private userService: UserService) {}
-  ngOnDestroy(): void {
-    this.activatedSub.unsubscribe();
-  }
-
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(
-      (didActivated) => {
-        this.userActivated = didActivated;
-      }
-    );
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 }
